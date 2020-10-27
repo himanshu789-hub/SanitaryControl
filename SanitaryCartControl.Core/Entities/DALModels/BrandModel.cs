f384a59718fd1d5ab45472d29f32dd8f84ef5a79
himanshu789-hub <khemanihimi78@gmail.com>
2020-10-27 15:12:13 +0530

    CLean Architecture

---
+++ SanitaryCartControl.Core/Entities/DALModels/BrandModel.cs
@@ -0,0 +1,9 @@
+namespace SanitaryContro.DALModels
+{
+    public class BrandModel
+    {
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public string Image{get;set;}
+    }
+}
