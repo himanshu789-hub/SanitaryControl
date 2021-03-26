b014471d52454c9cb17c6053b2e3363efb7b7879
himanshu789-hub <khemanihimi78@gmail.com>
2021-03-26 18:25:22 +0530

    Make Changes

--- SanitaryCartControl/ViewModels/LogInViewModel.cs
+++ SanitaryCartControl/ViewModels/LogInViewModel.cs
@@ -4,7 +4,7 @@ namespace SanitaryCartControl.ViewModels
     public class LogInViewModel
     {
         [Required]
-        [RegularExpression(@"^[^ ][A-Za-z_@0-9\.]*$")]
+        [RegularExpression(@"^(?=.{6,21}$)(?![_.@])(?!.*[_.@]{2})[a-zA-Z0-9@._]+(?<![@_.])$")]
         public string UserName{get;set;}
         [Required]
         [MinLength(6)]
--- SanitaryCartControl/ViewModels/RegisterViewModel.cs
+++ SanitaryCartControl/ViewModels/RegisterViewModel.cs
@@ -17,11 +17,13 @@ namespace SanitaryCartControl.ViewModels
         public string LastName{get;set;}
 
         [Required(ErrorMessage = "Please Specify A User Name")]
-        [RegularExpression(@"^[^ ][A-Za-_z@0-9\.]*$", ErrorMessage = @"A Username cannot contain any special character except @")]
+        [RegularExpression(@"^(?=.{6,21}$)(?![_.@])(?!.*[_.@]{2})[a-zA-Z0-9@._]+(?<![@_.])$", ErrorMessage = @"@_. must be in between and 0-9a-z are valid characters")]
         [MinLength(6,ErrorMessage="Please Enter Minimum Six Character Username")]
+        [MaxLength(21,ErrorMessage="Please Enter Maximum Twenty One Character Username")]
         public string UserName{get;set;}
+        
         [MinLength(6,ErrorMessage="Password Must Be Atleast 6 Character Long")]
-          [Required(ErrorMessage="Please Enter Password")]
+        [Required(ErrorMessage="Please Enter Password")]
         public string Password{get;set;}
         [Compare("Password",ErrorMessage="Pasword and Confimation Do Not Match")]
         public string ConfirmPassword{get;set;} 
